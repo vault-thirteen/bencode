d39d97a3059cb20643483d207c44e0a543293b66
Neverwinter Nights <v73o@yandex.com>
2021-09-23 18:34:15 +0300

    Licence and copyright.

--- errors.go
+++ errors.go
@@ -1,26 +1,3 @@
-// errors.go.
-
-//============================================================================//
-//
-// Copyright © 2018..2021 by McArcher.
-//
-// All rights reserved. No part of this publication may be reproduced,
-// distributed, or transmitted in any form or by any means, including
-// photocopying, recording, or other electronic or mechanical methods,
-// without the prior written permission of the publisher, except in the case
-// of brief quotations embodied in critical reviews and certain other
-// noncommercial uses permitted by copyright law. For permission requests,
-// write to the publisher, addressed “Copyright Protected Material” at the
-// address below.
-//
-//============================================================================//
-//
-// Web Site:		'https://github.com/neverwinter-nights'.
-// Author:			McArcher.
-// Web Site Address is an Address in the global Computer Internet Network.
-//
-//============================================================================//
-
 package bencode
 
 import "errors"
